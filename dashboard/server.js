e16ef7f037fdadda3cccf2850753b27984a77fcb
M1000gt <luizgustavogaivotto@gmail.com>
2026-06-22 00:50:28 -0300

    Atualização dasboard

--- dashboard/server.js
+++ dashboard/server.js
@@ -822,10 +822,7 @@ app.get('/', async (req, res) => {
         </section>
     `;
 
-  res.send(layout(conteudo, 'dashboard'));
-res.send(layout(conteudo, 'qr'));
-res.send(layout(conteudo, 'logs'));
-res.send(layout(conteudo, 'clientes'));
+    res.send(layout(content));
 });
 
 app.post('/restart/:pm2', async (req, res) => {
